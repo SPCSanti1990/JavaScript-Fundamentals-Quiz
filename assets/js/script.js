2eda39046b3c9a3fb7ee90da4e93716c9368bce9
SPCSanti1990 <81482804+SPCSanti1990@users.noreply.github.com>
2023-10-29 08:59:53 -0400

    questions and choices are displayable

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,26 +3,45 @@
 // Variables
 // user name, countdown, score
 let timeEl = document.querySelector(".time");
-let secondsLeft = 75;
+let secondsLeft = 60;
+let score = 0;
+let currentQuestion = 0;
+const questionContainer = document.getElementById("questionContainer");
+const questionText = document.getElementById("questionText");
+const choiceList = document.getElementById("choiceList");
+const startQuizButton = document.getElementById("startQuiz");
 
-// Class used to create question, answer, and choices
-class Question {
-    constructor(question, answer, choice1, choice2, choice3) {
-        this.question = question;
-        this.answer = answer;
-        this.choice1 = choice1;
-        this.choice2 = choice2;
-        this.choice3 = choice3;
+startQuizButton.addEventListener("click", startQuiz);
+
+// Quiz questions and answers
+
+const questions = [
+    {
+        question:"JavaScript is an ____ language?",
+        choices:["Object-Oriented", "Object-Based", "Procedural", "None of the above"],
+        CorrectAnswer:0
+    },
+    {
+        question:"Which of the following keywords is used to define a variable?",
+        choices:["let", "char", "def", "None of the above"],
+        answer:0
+    },
+    {
+        question:"Which of the following methods can be used to display data in some form using Javascript",
+        choices:["document.write()", "consol.log()", "window.alert", "All of the above"],
+        answer:3
+    },
+    {
+        question:"Which of the following methods is used to access HTML elements using Javascript?",
+        choices:["getElementbyId()", "getElementByClassName()", "Both A and B", "None of the above"],
+        answer:2
+    },
+    {
+        question:"Which of the following keywords is used to define constant variable?",
+        choices:["const", "var", "let", "constant"],
+        answer:0
     }
-}
-// Question objects
-const question1 = new Question("JavaScript is an ____ language?", "Object-Oriented", "Object-Based", "Procedural", "None of the above");
-const question2 = new Question("Which of the following keywords is used to define a variable?", "let", "char", "def", "None of the above");
-const question3 = new Question();
-const question4 = new Question();
-
-// array of objects containing questions, correct answers, and choices
-const questions = [question1, question2, question3, question4];
+];
 
 // function to add new users and high scores
 function highScore (name, score) {
@@ -39,19 +58,51 @@ function setTime() {
       if(secondsLeft === 0) {
         // Stops execution of action at set interval
         clearInterval(timerInterval);
-
       }
-  
     }, 1000);
   }
-// while loop until time runs out
-while (secondsLeft > 0) {
-    setTime();
 
+function startQuiz() {
+    startQuizButton.style.display = "none";
+     displayQuestion();
+     setTime();
+}
+
+function displayQuestion() {
+    const currentQuestionData = questions[currentQuestion];
+    questionText.textContent = currentQuestionData.question;
+    choiceList.innerHTML = "";
+
+    currentQuestionData.choices.forEach((choice, index) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = choice;
+        listItem.addEventListener("click", () => checkAnswer(index));
+        choiceList.appendChild(listItem);
+    });
 }
 
 // if question answer is right add 1 to score, display new question from array
-
+function checkAnswer(selectedIndex) {
+    const currentQuestionData = questions[currentQuestion];
+    if (selectedIndex === currentQuestionData.correctAnswer) {
+        score++;
+    }
 // if question answer is wrong subtract time, display new question from array
+    else if (selectedIndex !== currentQuestionData.correctAnswer) {
+        secondsLeft = secondsLeft - 10;
+    }
+    currentQuestion++;
+    if (currentQuestion < questions.length) {
+        displayQuestion();
+    } 
+    else {
+        endQuiz();
+    }
+}
 
 // when time runs out games ends and promps user for name to add to high score
+function endQuiz() {
+    questionContainer.style.display = "none";
+    timeLeftDisplay.textContent = "Time's up!";
+    alert(`Quiz Over! Your Score: ${score}/${questions.length}`);
+}
